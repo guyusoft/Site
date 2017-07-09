86bfd5b95e103f16956ab2e4778c368e3029c682
AlexSuDev <alexsudev@163.com>
2017-07-09 13:54:53 +0800

    Initialize

---
+++ GuyuSoft/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuyuSoft")]
+[assembly: AssemblyDescription("Guyu Soft")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Guyu Soft")]
+[assembly: AssemblyProduct("GuyuSoft")]
+[assembly: AssemblyCopyright("Copyright © GuyuSoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8b5f6dfc-9aba-48f4-9efe-04794dc69a05")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
